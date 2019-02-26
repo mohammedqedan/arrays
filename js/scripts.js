d799c313b3f9b17c77c0b430a237bb94f17a2aaa
Student <example@example.com>
2019-02-25 20:33:56 -0800

    added a working element

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
-    var favorite = [];
-    favorite = document.getElementById("favoriteThing1").value;
+    var items = [];
+    favorite = document.getElementById('favoriteThing1').value;
+    items.push(favorite);
     favorite = document.getElementById("favoriteThing2").value;
+    items.push(favorite);
     favorite = document.getElementById("favoriteThing3").value;
+    items.push(favorite);
     favorite = document.getElementById("favoriteThing4").value;
-    console.log(favorite);
+    items.push(favorite);
+    console.log(items);
   });
 });
